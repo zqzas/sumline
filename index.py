c806bc03a3728337e92604bffd5a2904a9349e6b
Arapat Alafate <arapat@arap.at>
2013-07-21 06:54:26 +0800

    added MongoDB usage sample

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 
 import web
 import os
-from bae.api import logging
+import sys
 
 urls = (
     '/', 'Hello' 
@@ -10,7 +10,8 @@ urls = (
 app_root = os.path.dirname(__file__)
 templates_root = app_root   # os.path.join(app_root, 'client')
 render = web.template.render(templates_root)
- 
+sys.path.insert(0, 'server')
+
 class Hello:        
     def GET(self):
       	q = web.input()
@@ -23,9 +24,18 @@ class Hello:
         if query:
             # TODO call server
             return render.search()
+        # self.test_mongodb() # How-to use MongoDB
         return render.the_index()
+
+    def test_mongodb(self):
+        from dbaccess import save, fetch
+        save({'a': 1, 'b':2}, "arapat")
+        return fetch({'a':1}, "arapat")
  
 app = web.application(urls, globals()).wsgifunc()
  
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
+
+
+
